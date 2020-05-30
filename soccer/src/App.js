d320fea0d7fe7bde63a98a18613e215f5bf9204e
nandu pokhrel <nandupokhrel@nandus-MacBook-Pro.local>
2020-05-29 21:03:58 -0400

    created endpoints using router

--- soccer/src/App.js
+++ soccer/src/App.js
@@ -1,46 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import Home from './Home'
+import Contact from './Contact'
+import Projects from './Projects'
+import About from './About'
+import {Route, Link} from 'react-router-dom';
+import NavBar from './Navbar'
 
+function App() {
+  return  <div className="App">
+    <NavBar />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/projects" component={Projects} />
+    <Route exact path="/contact" component={Contact} />
 
-const style = {
-  width: '100%',
-  height: '100%'
-}
-class Maps extends Component {
-  render (){
-    return (
-      <div className="App">
-        <header className="App-header">
-         
-          </header>
-          <Map google={this.props.google} 
-          style={style}
-          initialCenter={{
-            lat:  43.6426,
-            lng: -79.3871
-          }} zoom={16}>
-            
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />
-   
-          <InfoWindow onClose={this.onInfoWindowClose}>
-              {/* <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-              </div> */}
-          </InfoWindow>
-        </Map>
-        
-       
-      </div>
-    );
-
-  }
- 
+  </div>;
 }
 
-
-// Add your own API Key
-export default GoogleApiWrapper({
-  apiKey: ("AIzaSyBhoIICyXP7RGihA84YoU29VAiG5ZvQssY")
-})(Maps)
+export default App;
