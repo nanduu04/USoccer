1bbd512b70b7aaa05869cb9ee567709217c478c1
nanduu04 <33555126+nanduu04@users.noreply.github.com>
2020-05-30 13:42:34 -0400

    skeleton of event page

---
+++ src/Components/Events.js
@@ -0,0 +1,158 @@
+import React from 'react'
+import {Card, Button, Row, Col, Image, Container} from 'react-bootstrap'
+
+
+function Events(){
+    return (
+      <div> 
+      <h1> Here are some of the events near your area:</h1>
+  <Row className="events-container">
+  <Col>
+        <Card style={{ width: '18rem', height: '18rem' }}> 
+          <Card.Body className="events">
+              <Card.Title>Soccer</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">3522 Erindale Station Road</Card.Subtitle>
+              <Card.Text>
+              Required People: 5
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>     
+      </Col>
+
+      <Col>
+        <Card style={{ width: '18rem', height: '18rem' }}> 
+          <Card.Body className="events">
+              <Card.Title>Soccer</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">3522 Erindale Station Road</Card.Subtitle>
+              <Card.Text>
+              Required People: 5
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>     
+      </Col>
+
+      <Col>
+        <Card style={{ width: '18rem', height: '18rem' }}> 
+          <Card.Body className="events">
+              <Card.Title>Soccer</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">3522 Erindale Station Road</Card.Subtitle>
+              <Card.Text>
+              Required People: 5
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>     
+      </Col>
+      <Col>
+        <Card style={{ width: '18rem', height: '18rem' }}> 
+          <Card.Body className="events">
+              <Card.Title>Soccer</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">3522 Erindale Station Road</Card.Subtitle>
+              <Card.Text>
+              Required People: 5
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>     
+      </Col>
+
+      <Col>
+        <Card style={{ width: '18rem', height: '18rem' }}> 
+          <Card.Body className="events">
+              <Card.Title>Soccer</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">3522 Erindale Station Road</Card.Subtitle>
+              <Card.Text>
+              Required People: 5
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>     
+      </Col>
+
+    </Row>
+
+    <Row className="events-container">
+    <Col>
+        <Card style={{ width: '18rem', height: '18rem' }}> 
+          <Card.Body className="events">
+              <Card.Title>Soccer</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">3522 Erindale Station Road</Card.Subtitle>
+              <Card.Text>
+               Required People: 5
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>     
+      </Col>
+
+      <Col>
+        <Card style={{ width: '18rem', height: '18rem' }}> 
+          <Card.Body className="events">
+              <Card.Title>Soccer</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">3522 Erindale Station Road</Card.Subtitle>
+              <Card.Text>
+              Required People: 5
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>     
+      </Col>
+
+      <Col>
+        <Card style={{ width: '18rem', height: '18rem' }}> 
+          <Card.Body className="events">
+              <Card.Title>Soccer</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">3522 Erindale Station Road</Card.Subtitle>
+              <Card.Text>
+              Required People: 5
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>     
+      </Col>
+
+      <Col>
+        <Card style={{ width: '18rem', height: '18rem' }}> 
+          <Card.Body className="events">
+              <Card.Title>Soccer</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">3522 Erindale Station Road</Card.Subtitle>
+              <Card.Text>
+              Required People: 5
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>     
+      </Col>
+      <Col>
+        <Card style={{ width: '18rem', height: '18rem' }}> 
+          <Card.Body className="events">
+              <Card.Title>Soccer</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">3522 Erindale Station Road</Card.Subtitle>
+              <Card.Text>
+              Required People: 5
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>     
+      </Col>
+
+    </Row>
+  
+
+      </div>
+    )
+}
+
+export default Events;
