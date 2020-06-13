73ec7ce824a20768de4c34b18a44f89c9e810a72
nandu pokhrel <nandupokhrel@MacBook-Pro.local>
2020-06-12 20:10:51 -0400

    cleared few bug

--- src/Components/Find.js
+++ src/Components/Find.js
@@ -3,11 +3,7 @@ import React from 'react'
 function Find(){
     return (
         <div>
-<<<<<<< HEAD:src/Components/Find.js
             <h1> Find Events here</h1>
-=======
-            <h1> Profile</h1>
->>>>>>> aa202463e4ca5c7a0601fa8810f21912ef223089:src/Components/About.js
         </div>
     )
 }
