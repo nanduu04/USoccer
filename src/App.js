9155c94ff81e83e600f7c81dac656cea63a80280
nanduu04 <33555126+nanduu04@users.noreply.github.com>
2020-06-21 11:44:26 -0400

    notfoundpage

--- src/App.js
+++ src/App.js
@@ -7,20 +7,25 @@ import Contact from './Components/Contact'
 import Events from './Components/Events'
 import About from './Components/About'
 import Event from './Components/Event'
-import {Route} from 'react-router-dom';
+import NotFoundPage from './Components/NotFoundPage'
+import {Route, Switch} from 'react-router-dom';
 import NavBar from './Components/Navbar'
 
 function App() {
   return  <div className="App">
     <NavBar />
+    <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/about" component={About} />
-    <Route exact path="/events" component={Events} />
+    <Route exact path="/events/" component={Events} />
     <Route exact path="/contact" component={Contact} />
-    <Route exact path="/event" component={Event} />
-
+    <Route exact path="/event/" component={Event} />
+    <Route component={NotFoundPage} />
+    </Switch>
   </div>;
   
 }
+
+
 export default App;
 
